38368d620b280e3811ff8a2a79c3a4dc2f2ccfdf
Dilini Rajapaksha <dilini.rajapaksha.it@gmail.com>
2024-12-02 07:43:48 +1000

    disable spell check

--- src/components/QuizInput.tsx
+++ src/components/QuizInput.tsx
@@ -38,6 +38,7 @@ export const QuizInput: React.FC<QuizInputProps> = ({
         onChange={(e) => onChange(e.target.value)}
         onKeyPress={handleKeyPress}
         disabled={isDisabled}
+        spellCheck={false}
         placeholder="Type your answer here..."
         className="w-full px-4 py-2 text-lg border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 disabled:bg-gray-100"
       />
